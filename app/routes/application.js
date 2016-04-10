9f0feead6c5d85f394ecffeb3e0ae191ba99f2cc
Devin Weaver <suki@tritarget.org>
2016-04-10 00:16:22 -0400

    Fix wordlist AJAX in prod

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,6 +4,6 @@ export default Ember.Route.extend({
   ajax: Ember.inject.service(),
 
   model() {
-    return this.get('ajax').request('/wordlist.json');
+    return this.get('ajax').request('wordlist.json');
   }
 });
