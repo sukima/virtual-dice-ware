4fdd5cb72e5a1ff601e357b0433fc027244376d5
Devin Weaver <suki@tritarget.org>
2016-04-18 12:43:02 -0400

    Add dice view of entropy

--- app/controllers/entropy.js
+++ app/controllers/entropy.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   random: Ember.inject.service(),
 
+  sampleDice: Ember.computed('random.sample', function() {
+    return this.get('random.sample').split('')
+      .map(rndChar => rndChar.charCodeAt(0) % 6 + 1);
+  }),
+
   actions: {
     togglePolling() {
       const random = this.get('random');
