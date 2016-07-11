1cec81d5e03ed4aaf7fa629184ab1b8cfa522c06
Devin Weaver <suki@tritarget.org>
2016-07-11 09:20:38 -0400

    Convert style to DockYard

--- app/services/random.js
+++ app/services/random.js
@@ -1,37 +1,47 @@
 import Ember from 'ember';
 
+const {
+  Service, get, set,
+  computed, computed: { alias, equal }
+} = Ember;
+
 const SAMPLE_SIZE = 256;
 const REHASH_INTERVAL = 200;
 
-export default Ember.Service.extend({
+export default Service.extend({
   pollingEnabled: true,
-  initialEntropyPercent: Ember.computed('pollEntropyCount', function() {
-    return Math.min(this.get('pollEntropyCount') * 4, 100);
+
+  initialEntropyPercent: computed('pollEntropyCount', {
+    get() {
+      return Math.min(get(this, 'pollEntropyCount') * 4, 100);
+    }
   }),
-  initialEntropyComplete: Ember.computed.equal('initialEntropyPercent', 100),
+  initialEntropyComplete: equal('initialEntropyPercent', 100),
 
   init() {
-    this.set('pollEntropyCount', 0);
-    this.set('generator', uheprng());
+    this._super(...arguments);
+    set(this, 'pollEntropyCount', 0);
+    set(this, 'sample', '');
+    set(this, 'generator', uheprng());
     this.pollEntropy();
   },
 
   addEntropy(...garbage) {
-    const generator = this.get('generator');
+    const generator = get(this, 'generator');
     generator.addEntropy(...garbage);
-    this.set('sample', generator.string(SAMPLE_SIZE));
+    set(this, 'sample', generator.string(SAMPLE_SIZE));
+  },
+
+  dieRoll(sides=6) {
+    return get(this, 'generator')(sides) + 1;
   },
 
   pollEntropy() {
-    if (this.get('pollingEnabled')) {
+    if (get(this, 'pollingEnabled')) {
       this.addEntropy();
       this.incrementProperty('pollEntropyCount');
       Ember.run.later(this, 'pollEntropy', REHASH_INTERVAL);
     }
-  },
-
-  dieRoll(sides=6) {
-    return this.get('generator')(sides) + 1;
   },
 
   togglePolling() {
