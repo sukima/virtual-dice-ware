ed72ffe89592842b5466cff801c46680d6ebaf32
Devin Weaver <suki@tritarget.org>
2018-10-21 16:22:55 -0400

    Handle error loading wordlist

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,12 @@
-import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import MissingWordListError from '../utils/errors/missing-word-list';
 
 export default Route.extend({
   ajax: service(),
 
   model() {
-    return this.get('ajax').request('wordlist.json');
+    return this.get('ajax').request('wordlist.json')
+      .catch(error => { throw new MissingWordListError(error); });
   }
 });
